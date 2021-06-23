da492b714e6a10396a048d8a630fc4407492770b
Pranik Chainani <pranikc@gmail.com>
2021-06-23 15:21:23 -0400

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,25 @@
+class Doctor
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select{|appointment| appointment.doctor == self}
+    end
+
+    def patients
+        self.appointments.map{|appointment| appointment.patient}.uniq
+    end
+    
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,22 @@
+class Genre
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select{|song| song.genre == self}
+    end
+
+    def artists
+        get_all_songs = Song.all.select{|song| song.genre == self}
+        get_all_songs.map{|song| song.artist}
+    end
+end
