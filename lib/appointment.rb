2d877c03acafbef67bb63c135a38803b5a3f007d
Pranik Chainani <pranikc@gmail.com>
2021-06-23 15:21:20 -0400

    commit tree 9b3aa7e42ed0b541e8440d30e98ec9c881ee31a6

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,15 @@
+class Appointment
+    attr_accessor :patient, :doctor
+    @@all = []
+
+    def initialize(date, patient, doctor)
+        @patient = patient
+        @doctor = doctor
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
 
